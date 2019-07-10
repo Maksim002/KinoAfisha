7dde5610f99f7751b0b1c846e6a45869a54e07a6
efsef <inoplanee@mail.ru>
2019-07-10 15:49:09 +0600

    adra

--- app/src/main/java/com/example/kinoafisha/ViewHolder.java
+++ app/src/main/java/com/example/kinoafisha/ViewHolder.java
@@ -10,12 +10,17 @@ import com.bumptech.glide.Glide;
 
 public class ViewHolder extends RecyclerView.ViewHolder {
     private ImageView imageView;
-    private TextView textView;
+    private TextView name, id, url,vote,countVote,phone,address;
 
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
         imageView = itemView.findViewById(R.id.imageView);
-        textView = itemView.findViewById(R.id.textView);
+        name = itemView.findViewById(R.id.name);
+        url = itemView.findViewById(R.id.url);
+        vote = itemView.findViewById(R.id.vote);
+        countVote = itemView.findViewById(R.id.countVote);
+        phone = itemView.findViewById(R.id.phone);
+        address = itemView.findViewById(R.id.address);
     }
 
     public void bind(UnMain value) {
@@ -23,6 +28,13 @@ public class ViewHolder extends RecyclerView.ViewHolder {
         .load(MainActivity.baseUrl + value.getImage())
         .into(imageView);
 
-        textView.setText(value.getName());
+        name.setText(value.getName());
+        url.setText(value.getUrl());
+        vote.setText(value.getVote());
+        countVote.setText(value.getCountVote());
+        phone.setText(value.getPhone());
+        address.setText(value.getAddress());
+
+
     }
 }
